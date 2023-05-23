7d890c88088077e9e2c8b651fe37f76665638e9a
Johannes Silvennoinen <johannes.silvennoinen44@gmail.com>
2023-05-23 19:11:09 +0200

    Add directory for utilities in business package. Add CDI in pom.xml Add Utility classes EmailSender, Hashing, SessionINfo, SystemInitializer and TrespassListener.

---
+++ src/main/java/dtt/business/utilities/SessionInfo.java
@@ -0,0 +1,9 @@
+package dtt.business.utilities;
+
+import jakarta.enterprise.context.SessionScoped;
+
+import java.io.Serializable;
+
+@SessionScoped
+public class SessionInfo implements Serializable {
+}

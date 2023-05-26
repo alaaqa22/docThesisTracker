b90d640b85790bb76db180fa32c439045b5daaf8
witka <witka@fim.uni-passau.de>
2023-05-26 18:33:00 +0200

    Javadoc for VoteDAO

--- src/main/java/dtt/dataAccess/repository/Postgres/VoteDAO.java
+++ src/main/java/dtt/dataAccess/repository/Postgres/VoteDAO.java
@@ -2,10 +2,10 @@ package dtt.dataAccess.repository.Postgres;
 
 import java.util.List;
 
+import dtt.dataAccess.exceptions.DataNotCompleteException;
 import dtt.dataAccess.exceptions.DataNotFoundException;
 import dtt.dataAccess.exceptions.InvalidInputException;
 import dtt.dataAccess.utilities.Transaction;
-import dtt.global.tansport.Circulation;
 import dtt.global.tansport.Vote;
 
 /**
@@ -21,47 +21,32 @@ public class VoteDAO implements dtt.dataAccess.repository.interfaces.VoteDAO {
 		
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
-	public void add(Vote vote, Circulation circulation, Transaction transaction) {
+	public void add(Vote vote, Transaction transaction) throws DataNotCompleteException, InvalidInputException {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
-	public void remove(Vote vote, Circulation circulation, Transaction transaction) {
+	public void remove(Vote vote, Transaction transaction) throws DataNotFoundException, InvalidInputException {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
-	public void update(Vote vote, Circulation circulation, Transaction transaction) {
+	public void update(Vote vote, Transaction transaction) throws DataNotFoundException, InvalidInputException {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/**
-	 * {@inheritDoc}
-	 * @return 
-	 */
 	@Override
-	public List<Vote> getVotes(Vote vote, Circulation circulation, Transaction transaction) {
+	public List<Vote> getVotes(Vote vote, Transaction transaction) {
+		// TODO Auto-generated method stub
 		return null;
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
-	public boolean findVote(Vote vote, Circulation circulation, Transaction transaction)
-			throws DataNotFoundException, InvalidInputException {
+	public boolean findVote(Vote vote, Transaction transaction) {
 		// TODO Auto-generated method stub
 		return false;
 	}

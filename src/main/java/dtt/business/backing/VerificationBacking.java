870fbce208abb4f795e595b5827927c9a3e600d7
alaaqa22 <52110813+alaaqa22@users.noreply.github.com>
2023-05-28 09:06:15 +0200

    Add Verification class

---
+++ src/main/java/dtt/business/backing/VerificationBacking.java
@@ -0,0 +1,16 @@
+package dtt.business.backing;
+
+/**
+ * Backing bean for verification.
+ */
+public class VerificationBacking {
+
+
+
+    /**
+     * Method to verify the email address.
+     */
+    public void verifyEmail(){
+
+    }
+}

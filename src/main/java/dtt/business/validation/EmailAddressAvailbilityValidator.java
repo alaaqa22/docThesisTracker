85fa52d53c8412fb99a2c6ea9441110e71fa0116
alaaqa22 <52110813+alaaqa22@users.noreply.github.com>
2023-06-18 15:46:23 +0200

    add Email-Address availability Validator

--- src/main/java/dtt/business/validation/EmailAddressAvailbilityValidator.java
+++ src/main/java/dtt/business/validation/EmailAddressAvailbilityValidator.java
@@ -1,7 +1,9 @@
 package dtt.business.validation;
 
 import dtt.dataAccess.repository.interfaces.UserDAO;
+import dtt.dataAccess.utilities.Transaction;
 import dtt.global.tansport.User;
+import jakarta.faces.application.FacesMessage;
 import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.validator.FacesValidator;
@@ -11,33 +13,49 @@ import jakarta.inject.Inject;
 
 /**
  * Email address validator that verifies if a provided email address exists within the database.
- *  @author Hadi Abou Hassoun
+ *
+ * @author Hadi Abou Hassoun
  */
 @FacesValidator("EmailAddressAvailbilityValidator")
 public class EmailAddressAvailbilityValidator implements Validator {
     @Inject
     private UserDAO userDAO; // UserDAO object for database access.
 
+    public static void main(String[] args) {
+
+
+    }
+
     /**
-     *
-     * @param context FacesContext for the request we are processing
+     * @param context   FacesContext for the request we are processing
      * @param component UIComponent we are checking for correctness
-     * @param value the value to validate
+     * @param value     the value to validate
      * @throws ValidatorException
      */
     @Override
-    public void validate (FacesContext context, UIComponent component, Object value) throws ValidatorException {
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        String email = (String) value;
+        if (!isEmailAddressAvailable(email)) {
+            throw new ValidatorException(new FacesMessage("The provided email address was not found."));
+
+        }
+
 
     }
 
     /**
      * check email availability
+     *
      * @param email The email address to check.
      * @return Return {@code true} if the email address is available, {@code false} otherwise.
      */
     private boolean isEmailAddressAvailable(String email) {
         User user = new User();
         user.setEmail(email);
-        return true;
+        Transaction transaction = new Transaction();
+        if (userDAO.findUserByEmail(user, transaction)) {
+            return true;
+        }
+        return false;
     }
 }

32bec0668cfa6e2fe2f76c7cbc7c0e597ae4adf1
witka <witka@fim.uni-passau.de>
2023-07-11 03:54:47 +0200

    Fixed Tests

--- src/test/java/selenium/basic/ChangeEmailPwTest.java
+++ src/test/java/selenium/basic/ChangeEmailPwTest.java
@@ -4,20 +4,22 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import selenium.setup.TestSetup;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
-
+import org.openqa.selenium.Keys;
 
 public class ChangeEmailPwTest {
 
@@ -38,53 +40,51 @@ public class ChangeEmailPwTest {
         driver.quit();
     }
 
-    @SuppressWarnings("deprecation")
     @Test
     public void changeEmailPw() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get(TestSetup.getBaseUrl());
-        driver.manage().window().setSize(new Dimension(1299, 692));
+        driver.manage().window().setSize(new Dimension(1382, 692));
         driver.findElement(By.id("login-form:email-itxt")).click();
         driver.findElement(By.id("login-form:password-iscrt"))
-                .sendKeys("Asdf&123");
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "Asdf&123");
         driver.findElement(By.id("login-form:email-itxt"))
-                .sendKeys("tom.test@test.com");
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "tom.test@test.com");
         driver.findElement(By.id("login-form:login-cbtn")).click();
-        driver.findElement(
-                By.cssSelector("#navbarForm\\3Aprofil-link > .nav-link"))
-                .click();
-        driver.findElement(By.id("j_idt50:email-itxt")).click();
-        driver.findElement(By.id("j_idt50:email-itxt"))
-                .sendKeys("bob.test@test.com");
-        driver.findElement(By.id("j_idt50:password-iscrt")).click();
-        driver.findElement(By.id("j_idt50:password-iscrt"))
-                .sendKeys("Asdf123&");
-        driver.findElement(By.id("j_idt50:save-cbtn")).click();
-        assertThat(driver.findElement(By.cssSelector("li > span")).getText(),
-                is("Ihre persönlichen Informationen wurden erfolgreich aktualisiert."));
+        driver.findElement(By.id("navbarForm:profil-link")).click();
+        driver.findElement(By.id("profile:email-itxt")).click();
+        driver.findElement(By.id("profile:email-itxt"))
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "bob.test@test.com");
+        driver.findElement(By.id("profile:password-iscrt")).click();
+        driver.findElement(By.id("profile:password-iscrt"))
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "Asdf123&");
+        driver.findElement(By.id("profile:save-cbtn")).click();
+        wait.until(ExpectedConditions
+                .presenceOfElementLocated(By.cssSelector(".alert")));
+        assertThat(driver.findElement(By.cssSelector(".alert")).getText(),
+                is("Neue Änderungen wurden erfolgreich aktualisiert."));
         driver.findElement(By.id("navbarForm:logout-cbtn")).click();
         driver.findElement(By.id("login-form:email-itxt")).click();
         driver.findElement(By.id("login-form:password-iscrt"))
-                .sendKeys("Asdf123&");
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "Asdf123&");
         driver.findElement(By.id("login-form:email-itxt"))
-                .sendKeys("bob.test@test.com");
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "bob.test@test.com");
         driver.findElement(By.id("login-form:login-cbtn")).click();
-        assertThat(driver.findElement(By.cssSelector("th > .text-center"))
-                .getText(), is("Alle Umläufe"));
-        driver.findElement(
-                By.cssSelector("#navbarForm\\3Aprofil-link > .nav-link"))
-                .click();
-        driver.findElement(By.id("j_idt50:email-itxt")).click();
-        driver.findElement(By.id("j_idt50:password-iscrt"))
-                .sendKeys("Asdf&123");
-        driver.findElement(By.id("j_idt50:email-itxt"))
-                .sendKeys("tom.test@test.com");
-        driver.findElement(By.id("j_idt50:save-cbtn")).click();
+        assertThat(driver.getCurrentUrl(), is(TestSetup.getBaseUrl()
+                + "views/authenticated/circulationslist.xhtml"));
+        driver.findElement(By.id("navbarForm:profil-link")).click();
+        driver.findElement(By.id("profile:email-itxt")).click();
+        driver.findElement(By.id("profile:password-iscrt"))
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "Asdf&123");
+        driver.findElement(By.id("profile:email-itxt"))
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "tom.test@test.com");
+        driver.findElement(By.id("profile:save-cbtn")).click();
         driver.findElement(By.id("navbarForm:logout-cbtn")).click();
         driver.findElement(By.id("login-form:email-itxt")).click();
         driver.findElement(By.id("login-form:password-iscrt"))
-                .sendKeys("Asdf&123");
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "Asdf&123");
         driver.findElement(By.id("login-form:email-itxt"))
-                .sendKeys("tom.test@test.com");
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "tom.test@test.com");
         driver.findElement(By.id("login-form:login-cbtn")).click();
         driver.findElement(By.id("navbarForm:logout-cbtn")).click();
     }
--- src/test/java/selenium/basic/ChangeProfileTest.java
+++ src/test/java/selenium/basic/ChangeProfileTest.java
@@ -4,19 +4,21 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 import selenium.setup.TestSetup;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ChangeProfileTest {
 
@@ -37,44 +39,48 @@ public class ChangeProfileTest {
         driver.quit();
     }
 
-    @SuppressWarnings("deprecation")
     @Test
     public void changeProfile() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get(TestSetup.getBaseUrl());
-        driver.manage().window().setSize(new Dimension(884, 692));
+        driver.manage().window().setSize(new Dimension(1382, 692));
         driver.findElement(By.id("login-form:email-itxt")).click();
         driver.findElement(By.id("login-form:password-iscrt"))
-                .sendKeys("password123");
+                .sendKeys("Asht123&");
         driver.findElement(By.id("login-form:email-itxt"))
-                .sendKeys("test@test.com");
+                .sendKeys("john@example.com");
         driver.findElement(By.id("login-form:login-cbtn")).click();
-        driver.findElement(
-                By.cssSelector("#navbarForm\\3Aprofil-link > .nav-link"))
-                .click();
-        driver.findElement(By.id("j_idt50:firstName-itxt")).click();
-        driver.findElement(By.id("j_idt50:firstName-itxt")).sendKeys("Bob");
-        driver.findElement(By.id("j_idt50:lastName-itxt")).click();
-        driver.findElement(By.id("j_idt50:lastName-itxt")).sendKeys("Bob");
-        driver.findElement(By.id("j_idt50:birthDate-itxt")).click();
-        driver.findElement(By.id("j_idt50:birthDate-itxt"))
-                .sendKeys("20.04.1970");
-        driver.findElement(By.id("j_idt50:save-cbtn")).click();
-        driver.findElement(By.cssSelector("li > span")).click();
-        assertThat(driver.findElement(By.cssSelector("li > span")).getText(),
-                is("Ihre persönlichen Informationen wurden erfolgreich aktualisiert."));
+        driver.findElement(By.id("navbarForm:profil-link")).click();
+        driver.findElement(By.id("profile:firstName-itxt")).click();
+        driver.findElement(By.id("profile:firstName-itxt")).click();
+        driver.findElement(By.id("profile:firstName-itxt"))
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "Bob");
+        driver.findElement(By.id("profile:lastName-itxt")).click();
+        driver.findElement(By.id("profile:lastName-itxt"))
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "Wild");
+        driver.findElement(By.id("profile:birthDate-itxt")).click();
+        driver.findElement(By.id("profile:birthDate-itxt"))
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "20.04.1970");
+        driver.findElement(By.id("profile:save-cbtn")).click();
+        wait.until(ExpectedConditions
+                .presenceOfElementLocated(By.cssSelector(".alert")));
+        assertThat(driver.findElement(By.cssSelector(".alert")).getText(),
+                is("Neue Änderungen wurden erfolgreich aktualisiert."));
         driver.findElement(By.id("navbarForm:homepage-link")).click();
-        driver.findElement(
-                By.cssSelector("#navbarForm\\3Aprofil-link > .nav-link"))
-                .click();
-        driver.findElement(By.id("j_idt50:firstName-itxt")).sendKeys("Test");
-        driver.findElement(By.id("j_idt50:lastName-itxt")).click();
-        driver.findElement(By.id("j_idt50:lastName-itxt")).sendKeys("Test");
-        driver.findElement(By.id("j_idt50:birthDate-itxt")).click();
-        driver.findElement(By.id("j_idt50:birthDate-itxt")).click();
-        driver.findElement(By.id("j_idt50:birthDate-itxt"))
-                .sendKeys("20.04.1969");
-        driver.findElement(By.id("j_idt50:save-cbtn")).click();
-        driver.findElement(By.cssSelector("li > span")).click();
+        driver.findElement(By.id("navbarForm:profil-link")).click();
+        driver.findElement(By.id("profile:firstName-itxt"))
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "John");
+        driver.findElement(By.id("profile:lastName-itxt")).click();
+        driver.findElement(By.id("profile:lastName-itxt"))
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "Doe");
+        driver.findElement(By.id("profile:birthDate-itxt")).click();
+        driver.findElement(By.id("profile:birthDate-itxt")).click();
+        driver.findElement(By.id("profile:birthDate-itxt"))
+                .sendKeys(Keys.chord(Keys.CONTROL, "a"), "20.04.1969");
+        driver.findElement(By.id("profile:save-cbtn")).click();
+        wait.until(ExpectedConditions
+                .presenceOfElementLocated(By.cssSelector(".alert")));
+        driver.findElement(By.cssSelector(".alert")).click();
         driver.findElement(By.id("navbarForm:logout-cbtn")).click();
     }
 }

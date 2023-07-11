7171cd9a9b896015ef15e8b6c28ae0cee3d8ee04
alaaqa22 <52110813+alaaqa22@users.noreply.github.com>
2023-07-11 11:50:45 +0200

    add facultyTest add/remove fix modifyCircTest

--- src/test/java/selenium/basic/ModifyCircTest.java
+++ src/test/java/selenium/basic/ModifyCircTest.java
@@ -1,6 +1,6 @@
-
 package selenium.basic;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
@@ -18,40 +18,44 @@ import selenium.setup.TestSetup;
 
 import java.util.*;
 
-public class ModifyCircTest {
-  private WebDriver driver;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
-  @Before
-  public void setUp() {
-    TestSetup.setup();
-    driver =TestSetup.getDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
-  }
-  @After
-  public void tearDown() {
-    TestSetup.tearDown();
-    driver.quit();
-  }
-  @Test
-  public void modifyCircTest() {
-    driver.get(TestSetup.getBaseUrl());
-      driver.manage().window().setSize(new Dimension(1440, 823));
-      driver.findElement(By.id("login-form:email-itxt")).sendKeys("test@test.com");
-      driver.findElement(By.id("login-form:password-iscrt")).sendKeys("password123");
-      driver.findElement(By.id("login-form:login-cbtn")).click();
-      driver.findElement(By.id("dataForm:cir-dt:13:titel-otxt")).click();
-      driver.findElement(By.id("circulationDetails:end-date-itxt")).click();
-      driver.findElement(By.id("circulationDetails:end-date-itxt")).click();
-      {
-          WebElement element = driver.findElement(By.id("circulationDetails:end-date-itxt"));
-          Actions builder = new Actions(driver);
-          builder.doubleClick(element).perform();
-      }
-      driver.findElement(By.id("circulationDetails:end-date-itxt")).sendKeys("10 Nov 2023");
-      driver.findElement(By.id("circulationDetails:modify-cbtn")).click();
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-  }
+public class ModifyCircTest {
+    JavascriptExecutor js;
+    private WebDriver driver;
+    private Map<String, Object> vars;
+
+    @Before
+    public void setUp() {
+        TestSetup.setup();
+        driver = TestSetup.getDriver();
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<String, Object>();
+    }
+
+    @After
+    public void tearDown() {
+        TestSetup.tearDown();
+        driver.quit();
+    }
+
+    @Test
+    public void modifyCircTest() {
+        driver.get(TestSetup.getBaseUrl());
+        driver.manage().window().setSize(new Dimension(1440, 823));
+        driver.findElement(By.id("login-form:email-itxt")).sendKeys("test@test.com");
+        driver.findElement(By.id("login-form:password-iscrt")).sendKeys("password123");
+        driver.findElement(By.id("login-form:login-cbtn")).click();
+        driver.findElement(By.id("dataForm:cir-dt:0:titel-otxt")).click();
+        driver.findElement(By.id("circulationDetails:end-date-itxt")).click();
+        WebElement inputField = driver.findElement(By.id("circulationDetails:end-date-itxt"));
+        inputField.clear();
+        driver.findElement(By.id("circulationDetails:end-date-itxt")).sendKeys("28 Dec 2023");
+        driver.findElement(By.id("circulationDetails:modify-cbtn")).click();
+        String expectedMessage = "Umlauf wurde geändert.";
+        String actualMessage = driver.findElement(By.cssSelector(".alert")).getText();
+        assertEquals(expectedMessage, actualMessage);
+
+    }
 }
 
